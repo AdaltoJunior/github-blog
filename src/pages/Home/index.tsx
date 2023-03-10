fb7aa41828f379724c44b3cc6104895a3b59559d
Adalto Junior <mendesrochajunior@gmail.com>
2023-03-10 18:50:59 -0300

    feat: add search and post cards to Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,33 @@
 import { Profile } from '../../components/Profile'
-import { HomeContainer } from './styles'
+import { PostCard } from '../../components/PostCard'
+
+import {
+  HomeContainer,
+  PostContainer,
+  SearchContainer,
+  SearchCounter,
+  SearchHeader,
+  SearchHeading,
+} from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <Profile />
+
+      <SearchContainer>
+        <SearchHeader>
+          <SearchHeading>Publicações</SearchHeading>
+          <SearchCounter>6 publicações</SearchCounter>
+        </SearchHeader>
+        <input type="text" placeholder="Buscar conteúdo" />
+      </SearchContainer>
+
+      <PostContainer>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <PostCard key={index} />
+        ))}
+      </PostContainer>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,4 +5,56 @@ export const HomeContainer = styled(Container).attrs({
   as: 'main',
 })`
   margin-top: -5.5rem;
+  padding-bottom: 5rem;
+`
+
+export const SearchContainer = styled.div`
+  margin-top: 4.5rem;
+
+  input {
+    width: 100%;
+    padding: 0.75rem 1rem;
+    color: ${({ theme }) => theme.colors.text};
+    background-color: ${({ theme }) => theme.colors.input};
+    border: 1px solid ${({ theme }) => theme.colors.border};
+    border-radius: 6px;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.colors.label};
+    }
+
+    &:focus {
+      outline: 0;
+      border-color: ${({ theme }) => theme.colors.accent};
+    }
+  }
+`
+
+export const SearchHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+  margin-bottom: 0.75rem;
+`
+
+export const SearchHeading = styled.h2`
+  color: ${({ theme }) => theme.colors.subtitle};
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+  font-weight: 700;
+`
+
+export const SearchCounter = styled.span`
+  color: ${({ theme }) => theme.colors.span};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`
+
+export const PostContainer = styled.div`
+  display: grid;
+  gap: 2rem;
+  margin-top: 3rem;
+
+  @media (min-width: ${({ theme }) => theme.screens.md}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `
